f84db17bb426ef1b079a03da4c30211cf03f03e6
ryota1563 <alfimy1563ro@gmail.com>
2024-03-18 13:33:40 +0900

    'コメント'

--- public/js/product.js
+++ public/js/product.js
@@ -1,8 +1,6 @@
 
 function sortEvent(){
 
-deleteEvent()
-
   $('#sorttable').tablesorter({
   headers: {
       0: {sorter:false},
@@ -36,18 +34,23 @@ let kagenStock = $('#kagen-stock').val();
             },
             dataType: 'html', //json形式で受け取る
 
-         }).done(function sortEvent(data){
+         }).done(function replaceWith(data){
            // console.log(data);
            let newTable = $(data).find('#products-table');
            $('#products-table').replaceWith(newTable);
 
 
-        }).fail(function(){
+        }
+
+        sortEvent()
+      ).fail(function(){
               //通信が失敗した時に実行される処理
               console.log('検索失敗')        //デフォルトソート
     })
   })
-  function deleteEven(){
+)
+
+  function deleteEvent(){
   $(".btn-dell").on('click', function (e){
 
     e.preventDefault();
@@ -83,4 +86,4 @@ let kagenStock = $('#kagen-stock').val();
                     })
   })
 }
-  }
+}
